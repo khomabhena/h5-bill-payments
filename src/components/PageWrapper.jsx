45a4a4f343af8f2738119d5a44607c9f93a84ddb
khomabhena <colwanymab@gmail.com>
2025-11-10 10:04:05 +0200

    Added padding to the bottom and sides

--- src/components/PageWrapper.jsx
+++ src/components/PageWrapper.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const PageWrapper = ({ children, className = '' }) => {
   return (
     <div className="min-h-screen bg-slate-100">
-      <div className={`min-h-screen mx-auto flex w-full max-w-4xl flex-col bg-white px-4 sm:px-6 lg:px-8 ${className}`}>
+      <div className={`min-h-screen mx-auto pb-8 flex w-full max-w-4xl flex-col bg-white px-4l sm:px-6l lg:px-4l ${className}`}>
         {children}
       </div>
     </div>
